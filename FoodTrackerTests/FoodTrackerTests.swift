3ea6dc4d10b073876ba75fa65e4e19dc744b791e
James <carlso70@purdue.edu>
2016-11-05 00:34:51 -0400

    Initial Table View menu started, cleaned up some of the tests for the meal class

--- FoodTrackerTests/FoodTrackerTests.swift
+++ FoodTrackerTests/FoodTrackerTests.swift
@@ -20,18 +20,18 @@ class FoodTrackerTests: XCTestCase {
         // Success cases
         
         let test1 = Meal(name: "test", photo: nil, rating: 5)
-        XCTAssertNotNil(test1)
+        XCTAssertNotNil(test1, "As long as name and rating are good, you can init with nil pic")
         
         let test2 = Meal(name: "Hi", photo: #imageLiteral(resourceName: "emptyStar"), rating: 0)
-        XCTAssertNotNil(test2)
+        XCTAssertNotNil(test2, "Name not nil, and rating > 0 is good")
         
         // Fail cases
         
         let test3 = Meal(name: "", photo: #imageLiteral(resourceName: "emptyStar"), rating: 10)
-        XCTAssertNil(test3)
+        XCTAssertNil(test3, "Empty name is bad")
         
         let test4 = Meal(name: "Jimmy", photo: nil, rating: -1)
-        XCTAssertNil(test4)
+        XCTAssertNil(test4, "Rating less than 0 is bad")
         
     }
     
